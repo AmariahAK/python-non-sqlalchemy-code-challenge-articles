a7ca3ca15d584bdeb85255b44b3e4d678c1506bf
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2024-06-01 15:17:35 +0300

    full test

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -21,21 +21,9 @@ class Article:
     def author(self):
         return self._author
 
-    @author.setter
-    def author(self, value):
-        if not isinstance(value, Author):
-            raise ValueError("Author must be of type Author.")
-        self._author = value
-
     @property
     def magazine(self):
         return self._magazine
-
-    @magazine.setter
-    def magazine(self, value):
-        if not isinstance(value, Magazine):
-            raise ValueError("Magazine must be of type Magazine.")
-        self._magazine = value
         
 class Author:
     def __init__(self, name):
@@ -61,18 +49,15 @@ class Author:
         return Article(self, magazine, title)
 
     def topic_areas(self):
-        return list(set(article.magazine.category for article in self.articles()))
+        categories = [article.magazine.category for article in self.articles()]
+        return list(set(categories)) if categories else None
 
 class Magazine:
     _all_magazines = []
 
     def __init__(self, name, category):
-        if not isinstance(name, str) or not (2 <= len(name) <= 16):
-            raise ValueError("Name must be a string between 2 and 16 characters.")
-        if not isinstance(category, str) or len(category) == 0:
-            raise ValueError("Category must be a non-empty string.")
-        self._name = name
-        self._category = category
+        self.name = name
+        self.category = category
         self.__class__._all_magazines.append(self)
 
     @property
@@ -102,12 +87,14 @@ class Magazine:
         return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        return [article.title for article in self.articles()]
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
         from collections import Counter
         author_counts = Counter(article.author for article in self.articles())
-        return [author for author, count in author_counts.items() if count > 2]
+        contributing_authors = [author for author, count in author_counts.items() if count > 2]
+        return contributing_authors if contributing_authors else None
 
     @classmethod
     def top_publisher(cls):
